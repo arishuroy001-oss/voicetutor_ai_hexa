da9fafc78d555b6142b1de66e49df30e0acc1e6f
arishuroy001-oss <arishuroy001@gmail.com>
2026-06-03 14:54:23 +0530

    Update constants.dart

--- voicetutor_ai/lib/utils/constants.dart
+++ voicetutor_ai/lib/utils/constants.dart
@@ -9,4 +9,25 @@ class Constants {
 
   static const int listenTimeoutSeconds = 10;
   static const int pauseTimeoutSeconds = 3;
+  static const int nextQuestionDelayMs = 1500;
+  static const String wakeWordPath = 'assets/hey_tutor.ppn';
+  static const String picovoiceAccessKey = 'YOUR_KEY_HERE';
+  static const String geminiApiKey = 'YOUR_GEMINI_KEY_HERE';
+  static const String geminiModel = 'gemini-pro';
+
+  static const List<String> subjects = [
+    'Mathematics',
+    'Science',
+    'English',
+    'History',
+    'Geography',
+  ];
+
+  static const List<String> iDontKnowKeywords = [
+    'i don\'t know',
+    'not sure',
+    'no idea',
+    'don\'t know',
+    'idk',
+  ];
 }
