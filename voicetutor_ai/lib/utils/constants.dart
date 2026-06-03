a93d9728ed46dcf0304930cd781bdfdb40402d3d
arishuroy001-oss <arishuroy001@gmail.com>
2026-06-03 14:41:24 +0530

    Add constants for language codes and timeouts

---
+++ voicetutor_ai/lib/utils/constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+  static const Map<String, String> langCodes = {
+    'English': 'en-IN',
+    'Hindi': 'hi-IN',
+    'Bengali': 'bn-IN',
+    'Tamil': 'ta-IN',
+    'Telugu': 'te-IN',
+  };
+
+  static const int listenTimeoutSeconds = 10;
+  static const int pauseTimeoutSeconds = 3;
+}
